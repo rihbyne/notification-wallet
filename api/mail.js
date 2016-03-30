8db7cf85130ccdaad8382f5c33b2e71a2a4709b1
swanand_searchtrade <swanand.searchtrade@gmail.com>
2016-03-30 11:29:27 +0530

    made notification functionality in notification server

--- api/mail.js
+++ api/mail.js
@@ -3,14 +3,16 @@
 "use strict";
 
 var notificationschema  = require('../model/notification_model.js');
-var notiMessageSchema   = require('../model/notification_message_model.js');
+//var notiMessageSchema   = require('../model/notification_message_model.js');
 var Mailgun             = require('mailgun-js');							// For Emails (Mailgun Module)
 var request             = require('request');                               // Request Module
+var crypt               = require("../config/crypt");			    		// Crypt Connectivity.
+
 var from_who            = 'donotreply@searchtrade.com';						// Sender of Email
 var api_key             = 'key-2b8f2419e616db09b1297ba51d7cc770';			// Api Key For Mailgun
 var domain              = 'searchtrade.com';								// Domain Name
 
-var ip                  = 'http://192.168.1.29:5000';
+var ip                  = 'http://192.168.2.12:5000';
 var mailgun             = new Mailgun({apiKey: api_key, domain: domain});	// Mailgun Object
 
 
@@ -55,75 +57,138 @@ module.exports.sendmail = function (mailinfo, res) {
 
 
 // PHP Mail Functionality in Node
-module.exports.sendPHPmail = function (req, res){
+module.exports.sendNotification = function (req, res){
     
     var to                  = req.body.to;
     var subject             = req.body.subject;
-    var message             = req.body.message;
+    var email_body          = req.body.email_body;
     
     var first_name          = req.body.first_name;
     var last_name           = req.body.last_name;
     var user_id             = req.body.user_id;
     var notification_body   = req.body.notification_body;
+	var notification_code	= req.body.notification_code;
     
-    var notification = first_name+" "+last_name+", "+notification_body;
-    
-    var mailOptions = {
-		from: 'Search Trade <donotreply@searchtrade.com>', 	// Sender address
-		to: to, 								            // List of Receivers
-		subject: subject, 		                            // Subject line
-		text: message,									    // Text
-		html: message
-	};
-    
-    mailgun.messages().send(mailOptions, function(err, cb){
+	var publicKey			= req.body.publicKey;
+	var signature			= req.body.signature;
+	
+	// Validate Public Key
+	if(publicKey=="" || publicKey== null || publicKey==undefined)
+	{
+		console.log('Public Key is Missing');
+		sendResponse(req, res, 200, 1, "Mandatory field not found");
+		return;
+	}
+
+	// Validate Signature
+	if(signature=="" || signature== null || signature==undefined)
+	{
+		console.log('Signature is Missing');
+		master.sendResponse(req, res, 200, 1, "Mandatory field not found");
+		return;
+	}
+	
+	var notification 		= first_name+" "+last_name+", "+notification_body;
+	
+	var query = {publicKey:publicKey,token:true};
+	
+	request.post({
+                                
+        url: ip+'/api/getPvtKey',
+        body: query,
+        json: true,
+        headers: {"content-type": "application/json"}
+
+    },function optionalCallback(err, httpResponse, body){
+
+        if (err)
+        {
+            return console.error('Curl request Failed for register api: \n', err);
+        }
         
-         // Error In Sending Email
-        if (err) {
+		if(body.errCode == -1)
+		{
+			var privateKey = body.errMsg;
+			var text = 'to='+to+'&subject='+subject+'&first_name='+first_name+'&last_name='+last_name+'&user_id='+user_id+'&notification_code='+notification_code+'&publicKey='+publicKey;
+			
+			crypt.validateSignature(text, signature, privateKey, function(isValid){
             
-            console.log('Mail Not Sent');
-            console.log(err);
-            sendResponse(req, res, 200, 29, "Email Sending Error");
-            return;
+				// Signature Not Matched
+				if (!isValid)
+				{
+					console.log('Invalid Signature');
+					sendResponse(req, res, 200, 14, "Invalid Signature");
+					return;
+				}
+				
+				else
+				{
+					if(1&parseInt(notification_code))
+					{
+						console.log("Send Email");
+						
+						var mailOptions = {
+							from: 'Search Trade <donotreply@searchtrade.com>', 	// Sender address
+							to: to, 								            // List of Receivers
+							subject: subject, 		                            // Subject line
+							text: email_body,									// Text
+							html: email_body
+						};
+					
+						mailgun.messages().send(mailOptions, function(err, cb){
+							
+							//Error In Sending Email
+							if (err) {
+								
+								console.log('Mail Not Sent');
+								console.log(err);
+								sendResponse(req, res, 200, 29, "Email Sending Error");
+								return;
 
-        } else {
-            
-            console.log('Mail Sent Successfully');
-            
-            getNotificationStatus(to, function(result){
-                
-                //console.log('Status : '+result.notification_status);
-                
-                if(result.notification_status)
-                {
-                    var notification_message = new notiMessageSchema({ 
-                        user_id: user_id, 	            // User Id
-                        message: notification	        // Text
-                    });
+							}
+							
+							console.log('Mail Sent Successfully');
+	
+						});
+					}
 
-                    notification_message.save(function(err){
+					if(2&parseInt(notification_code))
+					{
+						console.log("Send SMS");		
+					}
+					
+					if(4&parseInt(notification_code))
+					{
+						console.log("Push Notification");		
+					}
+					
+					var notification_message = new notificationschema.notification_msg({ 
+						user_id: user_id, 	                    // User Id
+						notification_body: notification	        // Text
+					});
 
-                        if(err)
-                        {
-                          console.log(err);
-                          return err;
-                        }
+					notification_message.save(function(err){
 
-                        console.log('Saved SuccessFully');
-                        sendResponse(req, res, 200, -1, "Success");
+						if(err)
+						{
+						  console.log(err);
+						  return err;
+						}
 
-                    });
-                    
-                }
-                
-                else
-                {
-                    sendResponse(req, res, 200, -1, "Success");
-                }
-                
-            })
-            
-        }    
+						console.log('Saved SuccessFully');
+						sendResponse(req, res, 200, -1, "Success");
+
+					});
+					
+				}
+				
+			})
+		}
+		
+		else
+		{
+			master.sendResponse(req, res, 200, body.errCode, body.errMsg);
+		}
         
     });
     
--- app.js
+++ app.js
@@ -7,9 +7,9 @@ var server              = require('http').Server(app);
 var bodyParser          = require('body-parser');
 var notification        = require('./api/notification.js');  
 var mailer              = require('./api/mail.js');                     // Mail Functionality
-var morgan    			= require('morgan');            		// Log To Console
+var morgan    			= require('morgan');            				// Log To Console
 
-app.use(morgan('dev'));									// Morgan To log Request To Console
+app.use(morgan('dev'));													// Morgan To log Request To Console
 
 app.use(bodyParser.urlencoded({extended: true}));
 app.use(bodyParser.json());
@@ -24,7 +24,7 @@ app.post('/secure/sendVerificationEmail', notification.sendVerificationEmail);
 app.post('/secure/sendforgotpassword', notification.sendforgotpassword);
 app.post('/secure/changePassEmail', notification.changePassEmail);
 app.post('/secure/resettedConfirmation', notification.resettedConfirmation);
-app.post('/secure/sendMail', mailer.sendPHPmail);
+app.post('/secure/sendNotification', mailer.sendNotification);
 app.post('/secure/getNotificationStatus', mailer.getNotificationStatus);
 
 app.post('/secure/getMyNotification', notification.getMyNotification);
